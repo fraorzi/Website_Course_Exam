0fa191de3b9184d7e1930eaa7a6d5e7f8226b4ef
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2024-01-03 05:13:45 +0100

    Remove course data from Home, defer to CourseDescription

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,10 +2,14 @@ import React from 'react';
 import CourseDescription from '../../components/CourseDescription/CourseDescription';
 
 function Home() {
+    const courseIds = [1, 2, 3];
+
     return (
         <div>
             <h1>Featured Courses</h1>
-            <CourseDescription />
+            {courseIds.map(courseId => (
+                <CourseDescription key={courseId} courseId={courseId} />
+            ))}
         </div>
     );
 }
